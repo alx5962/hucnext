6e8f152f808b9e6a06ee57b4565ce55001430ca4
alx5962 <alx5962@gmail.com>
2026-08-16 14:46:01 +0200

    fixed sprite collision

--- pce-studio/appData/engine/pcevm/src/collision.c
+++ pce-studio/appData/engine/pcevm/src/collision.c
@@ -1,5 +1,6 @@
 #define PCE_COLLISION_C 1
 #include "include/collision.h"
+#include "include/actor.h"
 
 int g_collision_width = 32;
 int g_collision_height = 28;
@@ -16,9 +17,17 @@ int collision_check_point(int px, int py) {
 }
 
 int collision_check_box(int x, int y) {
-    if (collision_check_point(x + 2, y + 8) != COLLISION_NONE) return 1;
-    if (collision_check_point(x + 13, y + 8) != COLLISION_NONE) return 1;
-    if (collision_check_point(x + 2, y + 15) != COLLISION_NONE) return 1;
-    if (collision_check_point(x + 13, y + 15) != COLLISION_NONE) return 1;
+    int y_top, y_bottom;
+    if (g_actor_size[0] == SZ_16x32) {
+        y_top = y + 16;
+        y_bottom = y + 23;
+    } else {
+        y_top = y + 8;
+        y_bottom = y + 15;
+    }
+    if (collision_check_point(x + 2, y_top) != COLLISION_NONE) return 1;
+    if (collision_check_point(x + 13, y_top) != COLLISION_NONE) return 1;
+    if (collision_check_point(x + 2, y_bottom) != COLLISION_NONE) return 1;
+    if (collision_check_point(x + 13, y_bottom) != COLLISION_NONE) return 1;
     return 0;
 }
